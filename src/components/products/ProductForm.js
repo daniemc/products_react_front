0952517003e23dfd7fae9fbecbb1ab20cb2baeb3
daniel.meneses <daniel.meneses@cuemby.com>
2020-02-25 19:09:14 -0500

    Add axios to project Add create product feature

---
+++ src/components/products/ProductForm.js
@@ -0,0 +1,45 @@
+import React, { Component } from 'react';
+import { Button, Form, FormGroup, Label, Input } from 'reactstrap';
+class ProductForm extends Component {
+    constructor(props) {
+        super(props);
+        this.state = { 
+            name: ''
+         }
+         this.onInputChange = this.onInputChange.bind(this)
+         this.onCallAction = this.onCallAction.bind(this)
+    }
+
+    onInputChange(e) {
+        this.setState({
+            [e.target.name]: e.target.value
+        });
+    }
+
+    onCallAction() {
+        this.props.action(this.state);
+    }
+
+    render() { 
+        const { name } = this.state
+        return ( 
+            <Form>
+                <FormGroup>
+                    <Label for="product_name">Name</Label>
+                    <Input 
+                        id="product_name" 
+                        type="text" 
+                        name="name" 
+                        value={name} 
+                        placeholder="Enter product name"
+                        onChange={this.onInputChange} />
+                </FormGroup>   
+                <Button onClick={this.onCallAction}>
+                    { this.props.mode === 'create' ? 'Save' : 'Update' }
+                </Button>             
+            </Form>
+         );
+    }
+}
+ 
+export default ProductForm;
--- src/components/products/ProductsNew.js
+++ src/components/products/ProductsNew.js
@@ -1,13 +1,33 @@
 import React, { Component } from 'react';
+import ProductForm from './ProductForm';
+import { Row, Col } from 'reactstrap';
+import axios from 'axios'
+
 class ProductsNew extends Component {
     constructor(props) {
         super(props);
         this.state = {  }
     }
+
+    createProduct(data) {
+        axios.post('http://localhost:5000/products/add', data)
+            .then(resp => {
+                console.log(resp);
+            })
+            .catch(err => console.log(err));
+    }
+
     render() { 
         return ( 
             <div>
-                New product component
+                <Row>
+                    <Col sm="12" md={{ size: 6, offset: 3 }}>
+                        <ProductForm
+                            mode="create"
+                            action={this.createProduct}
+                        />
+                    </Col>
+                </Row>                
             </div>
          );
     }
